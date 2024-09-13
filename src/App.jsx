c2d4c651463147cd07334c849e867af3c5d1a407
Thamaru <thamarujalthotage1@gmail.com>
2024-09-13 22:30:44 +0530

    Welcome Page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WelcomePage from './Pages/Welcome Page/Welcome Page';
+
+function App() {
+  return (
+    <div>
+      <WelcomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Welcome Page/Welcome Page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Welcome Page.css';
+import f1Logo from '../Welcome Page/Welcome Page Images/f1_logo.png'
+import f1RedLogo from '../Welcome Page/Welcome Page Images/F1_red.png'
+
+const WelcomePage = () => {
+    return (
+        <nav className="WelcomePage">
+            <div className='note'>Welcome To The Ultimate Racing Spectacle</div>
+            <div><img className='f1logo' src={f1RedLogo} alt="" /></div>
+            <div className='clickButtons'>
+                <div className='signInButton'>Log In</div>
+                <div className='signUpButton' >Sign up</div>
+            </div>
+            <div className='partners'>
+                <img className='f1WhiteLogo' src={f1Logo} alt="" />
+                <p>Formula One World Championship Limited (2003-2024)</p>
+            </div>
+        </nav>
+    );
+};
+
+export default WelcomePage;
